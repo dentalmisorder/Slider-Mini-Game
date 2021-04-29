b86a621a6d64b96cbf54661cc472678c6e38f3e6
Денис Бондаренко <denys.bondarenko@nure.ua>
2021-04-29 17:58:17 +0300

    Base abstract class for pooling gameobjs or any <T> objects

---
+++ Assets/Scripts/Base/Pool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class  Pool<T> : MonoBehaviour
+{
+    protected Predicate<T> _predicate;
+    protected Queue<T> _pool;
+
+    public Pool()
+    {
+        _pool = new Queue<T>();
+        _predicate = new Predicate<T>(IsTrue);
+    }
+
+    public abstract T Spawn(Vector3 spawnPos, Quaternion rotation, Predicate<T> predicate);
+
+    public abstract bool IsTrue(T objToCheck);
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ namespace Slider.Gameplay
 {  
     public class Player : MonoBehaviour
     {
-        public ChangableValues changableValues;
+        public ChangableValues settings;
 
         private Camera _cam;
         private Vector3 viewportPoint;
@@ -20,28 +20,28 @@ namespace Slider.Gameplay
 
         private void Move(Vector2 screenPosition)
         {
-            Vector3 viewportPoint = _cam.WorldToViewportPoint(transform.position);
+            viewportPoint = _cam.WorldToViewportPoint(transform.position);
 
-            Debug.Log($"Viewport.x {viewportPoint.x}");
-
-            if(screenPosition.x > Screen.width / 2 && viewportPoint.x < 1f)
-                transform.position += Vector3.right * changableValues.moveSpeed * Time.deltaTime;
-            else if(screenPosition.x < Screen.width / 2 && viewportPoint.x > 0)
-                transform.position += Vector3.left * changableValues.moveSpeed * Time.deltaTime; 
+            if(screenPosition.x > Screen.width / 2 && viewportPoint.x + settings.moveBorder < 1f)
+                transform.position += Vector3.right * settings.moveSpeed * Time.deltaTime;
+            else if(screenPosition.x < Screen.width / 2 && viewportPoint.x - settings.moveBorder > 0)
+                transform.position += Vector3.left * settings.moveSpeed * Time.deltaTime; 
         }
 
         private void OnTriggerEnter(Collider other) => GameOver();
 
-        //TODO:
+        //TODO: Restart Menu
         private void GameOver()
         {
+            Destroy(this.gameObject);
             Debug.Log("<color=yellow>[Player]</color> Its over, Anakin");
         }
 
         [System.Serializable]
         public class ChangableValues
         {
-            public float moveSpeed;
+            public float moveSpeed = 4f;
+            [Range(0.05f, 0.4f)] public float moveBorder = 0.1f;
         }
     }
 }
